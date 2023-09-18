0dba015a8dec7a8c78f22fd86b7e7d0de33cf134
Lauren Cavanaugh <laurencavanaugh@Laurens-MBP.lan>
2023-09-17 22:04:38 -0700

    added dashboard overview page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export function MainNav({
     const routes = [
         {
             href: `/${params.storeId}`, 
-            label: 'Home',
+            label: 'Overview',
             active: pathname === `/${params.storeId}`,
         },
         {
